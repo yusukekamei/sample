c68c97b1373d96f8f9dcf7ccbd242aad7d4fadc7
Yusuke Kamei <yusuke.kamei@o-05910-mac.local>
2015-09-03 10:12:08 +0900

    create sort.pl

---
+++ chapter07/sort.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+my @array = ('bbb','ccc','aaa');
+my @rtn = sort @array;
+
+my $count = 0;
+foreach my $value (@rtn) {
+  printf "%d : [%s]\n", $count, $value;
+  ++$count;
+}
+
+foreach my $value (@array) {
+  printf "%d : [%s]\n", $count, $value;
+  
+}
+
